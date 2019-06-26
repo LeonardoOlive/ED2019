ba1f6887ebe8b51863dc4fe4647b9b3ab496fa02
Leonardo-Bruno <43361182+LeonardoOlive@users.noreply.github.com>
2019-06-25 21:33:15 -0300

    Update Selection.c

--- Ordenação em vetores/Selection.c
+++ Ordenação em vetores/Selection.c
@@ -1,31 +1,20 @@
 #include "xpaint.h"
 #include "xvet.h"
 
-//Arquivo que o professor ensinou em sala
 
 #define SWAP(a, b, tipo) do{tipo SWAP = a; a = b; b = SWAP;}while(0);
 
-//find inicio de menor na partição [inicio, fim[
-int find_indiceMenor(int vet[], int size, int inicio, int fim){
-    int imenor = inicio;
-    for(int i = inicio + 1; i < fim; i++ ){
-        if(vet[i] < vet[imenor]){
-            imenor = i;
-            //achar o amarelo(menor) e troca com o verde
-        xd_vet(vet, size, "gr", i, imenor);
-        }
-    }
-        return imenor;
-}
-
 void selection(int vet[], int size){
-    for(int i = 0; i < size; i++){
-        xs_partition(i, size-1);
-        int imenor = find_indiceMenor(vet, size, i, size);
-        //Pega o Vermelhor (maior valor) e troca com o menor(rosa)
-        xd_vet(vet, size, "mr", imenor, i);
-        SWAP(vet[i], vet[imenor], int);
-       // xd_vet(vet, size, "mr", imenor, i);
+    for(int i = 0; i < n ; i++){
+        int menor = i;
+        for(int j = i+1; j < n; j++){
+            if(vet[j] < vet[menor])
+                menor = j;
+        }
+        if(menor != i){
+            SWAP(vet[i],vet[menor]);
+        }
+
     }
 }
 
